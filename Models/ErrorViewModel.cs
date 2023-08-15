0acb9648bd5dc0c7cf96f0a1aa2d0d9102da42dd
Akash Paul <paulakash36@gmail.com>
2023-08-15 11:34:38 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MiePieShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
